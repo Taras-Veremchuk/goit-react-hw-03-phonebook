6dbeeccda86ef7e7a9d4021c10d3a8e8f59b02c7
Taras-Veremchuk <taras.veremm@gmail.com>
2023-05-05 17:55:19 +0200

    done hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,79 @@
-import React from "react";
-import contactData from './Сomponents/ContactData'
-import { ContactForm, ContactList, Filter } from "Сomponents/index";
-import shortid from "shortid";
-import {PhonTitle, ContTitle,  Container} from "./Сomponents/PhoneBook.styled"
-
-
- class App extends React.Component{
+import React from 'react';
+import { ContactForm, ContactList, Filter } from 'Сomponents/index';
+import shortid from 'shortid';
+import { PhonTitle, ContTitle, Container } from './Сomponents/PhoneBook.styled';
+class App extends React.Component {
   state = {
-     filter: '',
-  contacts: contactData,
-  }
-deleteContact = (id) => {
+    filter: '',
+    contacts: [],
+  };
+
+  deleteContact = id => {
     this.setState(prevState => ({
-    contacts: prevState.contacts.filter(contact=> contact.id!== id)
-  }))
-  }
-    addContact = ({name, number}) => {
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+  addContact = ({ name, number }) => {
     const contact = {
       id: shortid.generate(),
-      name:name,
+      name: name,
       number: number,
-    }
-    this.setState(({contacts}) => ({
-      contacts: [contact, ...contacts]
-    }))
-  }
-  changeFilter = (event) => {
-    this.setState({ filter: event.target.value })
-    this.filterContactList()
-  }
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+  changeFilter = event => {
+    this.setState({ filter: event.target.value });
+    this.filterContactList();
+  };
 
   filterContactList = () => {
-    const normalizedFilter = this.state.filter.toLowerCase().trim()
-    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
+    const normalizedFilter = this.state.filter.toLowerCase().trim();
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  componentDidMount() {
+    console.log('App componentDidMount');
+    const contacts = JSON.parse(localStorage.getItem('contactsList'));
+    if (contacts.length > 0) {
+      this.setState({ contacts: contacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('App componentDidUpdate');
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('ОБНОВИЛИСЯ КОНТАКТИ');
+      localStorage.setItem('contactsList', JSON.stringify(this.state.contacts));
+    }
   }
   render() {
+    const { contacts } = this.state;
     return (
       <Container>
-  <PhonTitle>Phonebook</PhonTitle>
-        <ContactForm addContact={this.addContact} contacts={ this.state.contacts} />
- <ContTitle>Contacts</ContTitle>
-        <Filter filter={ this.statefilter} onFilterChange={this.changeFilter} />
-        <ContactList visibleContacts={this.filterContactList()} deleteContact={this.deleteContact} />
-
-    </Container>
-  );}
-};
+        <PhonTitle>Phonebook</PhonTitle>
+        <ContactForm
+          addContact={this.addContact}
+          contacts={this.state.contacts}
+        />
+        {contacts.length > 0 ? (
+          <>
+            <ContTitle>Contacts</ContTitle>
+            <Filter
+              filter={this.statefilter}
+              onFilterChange={this.changeFilter}
+            />
+            <ContactList
+              visibleContacts={this.filterContactList()}
+              deleteContact={this.deleteContact}
+            />
+          </>
+        ) : (
+          <p message="No feedback given">No contacts added</p>
+        )}
+      </Container>
+    );
+  }
+}
 export default App;
